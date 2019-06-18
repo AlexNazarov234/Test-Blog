104a2b46937007a339bfe9e6b8d2c5306626a10a
Alex Nazarov <pokupka91@mail.ru>
2019-06-18 12:06:06 +0300

    refactoring: move sign_up to session_helper.rb

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,15 +3,8 @@ require 'capybara/rspec'
 
 feature "Account creation" do 
 	scenario "allows guest to create account" do 
-		visit new_user_registration_path
-
-		fill_in :user_email, :with => 'user@test.com'
-		fill_in :user_username, :with => 'mike123'
-		fill_in :user_password, :with => '111111'
-		fill_in :user_password_confirmation, :with => '111111'
-
-		click_button 'Sign up'
-
-		expect(page).to have_comment I18n.t 'devise.registrations.signed_up'
+		sign_up
+		expect(page).to have_comment I18n.t 'devise.registrations.signed_up'	
 	end
 end
+
